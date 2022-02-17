8b984a5617fad18a1b702ba1b0b79993ec7c4eb5
Amr Ahmed <amr_ahmed65@hotmail.com>
2022-02-17 15:44:57 +0200

    use resources instead of hardcoding

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
-  get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
-  get '/users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
-  get '/users', to: 'users#index', as: 'users'
-  get '/users/:user_id', to: 'users#show', as: 'user'
+
+  resources :users, only: [:index, :show] do
+
+    resources :posts, only: [:index, :show]
+
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+
+
   # Defines the root path route ("/")
+
   # root "articles#index"
+
 end
+
