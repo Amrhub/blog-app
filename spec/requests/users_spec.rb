dba52591f0800964b88f026e90d0805acec4723e
Amr Ahmed <amr_ahmed65@hotmail.com>
2022-02-17 21:26:07 +0200

    Test Users Controller

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'Users', type: :request do
+  describe 'GET /index' do
+    it 'get renders all users correctly' do
+      get '/users'
+      expect(response).to have_http_status(200)
+      expect(response.body).to render_template(:index)
+    end
+
+    it 'get renders the correct user' do
+      get '/users/42'
+      expect(response).to have_http_status(200)
+      expect(response.body).to include('42')
+    end
   end
 end
