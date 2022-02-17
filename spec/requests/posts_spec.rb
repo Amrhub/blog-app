55421a6b39c6975f6e7102727fbeaf18d2c81073
Amr Ahmed <amr_ahmed65@hotmail.com>
2022-02-17 21:25:59 +0200

    Test Posts Controller

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,6 +2,18 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    it 'get renders all posts page correctly' do
+      get '/users/120/posts'
+      expect(response).to have_http_status(200)
+      expect(response).to render_template(:index)
+      expect(response.body).to include('120')
+    end
+
+    it 'gets the correct blog for the given user id' do
+      get '/users/42/posts/21'
+      expect(response).to have_http_status(200)
+      expect(response.body).to include('42')
+      expect(response.body).to include('21')
+    end
   end
 end
