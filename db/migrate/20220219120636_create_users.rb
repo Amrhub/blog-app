487baa162f0aa1ee0d9e72c22769211b07a79ad1
Amr Ahmed <amr_ahmed65@hotmail.com>
2022-02-21 20:04:29 +0200

    Edit column names

--- db/migrate/20220219120636_create_users.rb
+++ db/migrate/20220219120636_create_users.rb
@@ -1,12 +1,26 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
+
   def change
+
     create_table :users do |t|
-      t.string :Name
-      t.text :Photo
-      t.text :Bio
-      t.integer :PostsCounter
+
+      t.string :name
+
+      t.text :photo
+
+      t.text :bio
+
+      t.integer :postsCounter
+
+
 
       t.timestamps
+
     end
+
   end
+
 end
+
+
+
