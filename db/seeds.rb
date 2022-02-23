c34e6f810b14879ceaa8cc8eb7bdd70aec026be1
Amr Ahmed <amr_ahmed65@hotmail.com>
2022-02-24 01:09:46 +0200

    Add default values to counters

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
-first_user = User.create(name: "Tom", photo: "https://unsplash.com/photos/F_-0BxGuVvo", bio: "Teacher from Mexico.")
-second_user = User.create(name: "Lilly", photo: "https://unsplash.com/photos/F_-0BxGuVvo", bio: "Teacher from Poland.")
+first_user = User.create(name: "Tom", photo: "https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/Item/48457/DIG010147_1._SX360_QL80_TTD_.jpg", bio: "Teacher from Mexico.")
+second_user = User.create(name: "Lilly", photo: "https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/Item/48457/DIG010147_1._SX360_QL80_TTD_.jpg", bio: "Teacher from Poland.")
 
 first_post = Post.create(user_id: first_user.id, title: "Hello", text: "This is my first post")
 second_post = Post.create(user_id: first_user.id, title: "Hello", text: "This is my second post")
