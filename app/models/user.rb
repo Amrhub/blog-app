91dd737f4380245ae2e94479690e3c8f9846823e
Amr Ahmed <amr_ahmed65@hotmail.com>
2022-02-23 20:23:14 +0200

    Add recent posts method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+  has_many :posts, class_name: 'Post'
+  has_many :likes, class_name: 'Like'
+  has_many :comments, class_name: 'Comment'
+
+  def recent_posts
+    Post.where(user_id: id).order(created_at: :desc).limit(3)
+  end
+
+  def update_posts_counter
+    posts_counter = Post.where(user_id: id).count
+    update(postsCounter: posts_counter)
+  end
 end
