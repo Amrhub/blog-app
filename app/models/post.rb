3d56fc93e875abdd971242ae5291e96e11b1eddc
Amr Ahmed <amr_ahmed65@hotmail.com>
2022-02-23 23:00:19 +0200

    Remove old one methods from posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,20 +2,15 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
   has_many :likes
+  after_save :update_post_counter
 
   def recent_comments
     Comment.where(post_id: id).order(created_at: :desc).limit(5)
   end
 
-  def update_comments_counter
-    comment_counter = Comment.where(post_id: id).count
+  private
 
-    update(commentsCounter: comment_counter)
-  end
-
-  def update_likes_counter
-    likes_counter = Like.where(post_id: id).count
-
-    update(likesCounter: likes_counter)
+  def update_post_counter
+    user.increment!(:postsCounter)
   end
 end
