bf58b047128b39af00cca992bf55bfceccd89a2a
Amr Ahmed <amr_ahmed65@hotmail.com>
2022-02-23 20:22:58 +0200

    Add methods update likes and commends counter

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,21 @@
 class Post < ApplicationRecord
-  belongs_to :users
+  belongs_to :user
+  has_many :comments
+  has_many :likes
+
+  def recent_comments
+    Comment.where(post_id: id).order(created_at: :desc).limit(5)
+  end
+
+  def update_comments_counter
+    comment_counter = Comment.where(post_id: id).count
+
+    update(commentsCounter: comment_counter)
+  end
+
+  def update_likes_counter
+    likes_counter = Like.where(post_id: id).count
+
+    update(likesCounter: likes_counter)
+  end
 end
