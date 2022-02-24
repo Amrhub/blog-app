6f2ff685828386c88b47685a082f7c642056bcaf
Amr Ahmed <amr_ahmed65@hotmail.com>
2022-02-24 22:29:40 +0200

    Handle redirects if parameters are not found

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,14 @@
 class PostsController < ApplicationController
   def index
-    @user = User.find(params[:user_id])
+    @user = User.find_by(id: params[:user_id])
     redirect_to users_path if @user.nil?
-    @posts = @user.posts
+    @posts = @user&.posts || 'No posts'
   end
 
   def show
-    @user = User.find(params[:user_id])
-
-    @post = Post.find(params[:id])
+    @user = User.find_by(id: params[:user_id])
+    redirect_to users_path if @user.nil?
+    @post = @user.posts.find_by(id: params[:id])
+    redirect_to user_posts_path(@user.id) if @post.nil?
   end
 end
