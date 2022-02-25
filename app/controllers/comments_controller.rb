4baae52b1b259bdf540bec6308e41edb48de5629
Amr Ahmed <amr_ahmed65@hotmail.com>
2022-02-26 00:38:43 +0200

    Add Functionality for Comments and likes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,24 @@
+# this class is for CommentsController
 class CommentsController < ApplicationController
-  def create
+  def new
+    @comment = Comment.new
   end
 
-  def new
+  def create
+    @user = current_user
+    @post_id = params[:post_id]
+    comment = Comment.new(comment_params.merge(user: @user, post_id: @post_id))
+    if comment.save
+      redirect_to user_post_path(@user.id, @post_id), success: "Your comment was successfully posted!"
+    else
+      flash.now[:danger] = "Your comment was not posted!"
+      render :new
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
   end
 end
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,4 +1,6 @@
 class LikesController < ApplicationController
   def create
+    Like.create(user: current_user, post_id: params[:post_id])
+    redirect_back(fallback_location: root_path)
   end
 end
