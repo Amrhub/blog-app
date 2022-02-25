dd7be80705ce4552117277fe0ca3895369c1cdc5
Amr Ahmed <amr_ahmed65@hotmail.com>
2022-02-25 22:22:25 +0200

    Add create new post functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,6 +12,25 @@ class PostsController < ApplicationController
     redirect_to user_posts_path(@user.id), danger: "No post was found by given id: #{params[:id]}" if @post.nil?
   end
 
-  def create; end
-  def new; end
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @current_user = current_user
+    redirect_to users_path, danger: "No user was found by given the id: #{params[:user_id]}" if @current_user.nil?
+    @post = @current_user.posts.new(post_params)
+    if @post.save
+      redirect_to user_path(@current_user.id), success: "Post was successfully created"
+    else
+      flash.now[:danger] = "Post was not created"
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text, :user_id)
+  end
 end
