bbcae819ee46c921d7503e4cac2bfeda70b4d3f6
Amr Ahmed <amr_ahmed65@hotmail.com>
2022-02-26 20:21:43 +0200

    Add specs to post models

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,63 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    User.create(id: 1, name: 'user1')
+  end
+
+  subject do
+    Post.new(title: 'Test title', user_id: 1, comments_counter: 1, likes_counter: 23, text: 'first subject, hello world')
+  end
+
+  before { subject.save }
+
+  it 'should have a title' do
+    subject.title = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'should have a title with length less than 250' do
+    subject.title = 'a' * 251
+    expect(subject).to_not be_valid
+  end
+
+  it 'should have a text' do
+    subject.text = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'should have a user_id' do
+    subject.user_id = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'should have a comments_counter' do
+    subject.comments_counter = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'should have a likes_counter' do
+    subject.likes_counter = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'should have a comments_counter greater than or equal to 0' do
+    subject.comments_counter = -1
+    expect(subject).to_not be_valid
+  end
+
+  it 'should have a likes_counter greater than or equal to 0' do
+    subject.likes_counter = -1
+    expect(subject).to_not be_valid
+  end
+
+  it 'recent_comments return recent 5 comments' do
+    subject.save
+    subject.comments << Comment.create(text: 'first comment', user_id: 1)
+    subject.comments << Comment.create(text: 'second comment', user_id: 1)
+    subject.comments << Comment.create(text: 'third comment', user_id: 1)
+    subject.comments << Comment.create(text: 'fourth comment', user_id: 1)
+    subject.comments << Comment.create(text: 'fifth comment', user_id: 1)
+    expect(subject.recent_comments.size).to eq(5)
+  end
 end
