a372a2581c964d4cd30a9d5d1b2f1f954c51e4ed
Amr Ahmed <amr_ahmed65@hotmail.com>
2022-02-26 17:04:17 +0200

    Add validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_many :posts, class_name: 'Post'
   has_many :likes, class_name: 'Like'
   has_many :comments, class_name: 'Comment'
+  validates :name, presence: true
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def recent_posts
     Post.where(user_id: id).order(created_at: :desc).limit(3)
