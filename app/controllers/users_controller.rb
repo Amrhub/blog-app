7889aaec6071817b89411dbd1a401bada7eaa484
Amrhub <amr_ahmed65@hotmail.com>
2022-03-05 21:58:45 +0200

    API endpoint list all users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,11 @@ class UsersController < ApplicationController
   def index
     @current_user = current_user
     @users = User.all
+
+    respond_to do |format|
+      format.html { render :index }
+      format.json { render json: @users }
+    end
   end
 
   def show
